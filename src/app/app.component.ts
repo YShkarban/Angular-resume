c4c8c0c3e38caf495b83bf146ab0eb57fb6810ce
yaroslav.shkarban <yarik.shkarban@gmail.com>
2025-05-18 20:04:31 +0200

    add language toggle feature and Polish translations; add new card with courses; update information about experience and skills; adjust CSS for better layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -30,11 +30,21 @@ export function HttpLoaderFactory(http: HttpClient) {
 })
 export class AppComponent {
   title = 'angular_resume';
+  lang: string = 'en'; // Default language
+
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang(this.lang);
+  }
 
   downloadCV() {
     let link = document.createElement('a');
     link.download = 'CV.pdf';
     link.href = '../assets/CV.pdf';
     link.click();
+  }
+
+  toggleLanguage(): void {
+    this.lang = this.lang === 'en' ? 'pl' : 'en'; // Toggle between 'en' and 'pl'
+    this.translate.use(this.lang); // Switch language dynamically
   }
 }
